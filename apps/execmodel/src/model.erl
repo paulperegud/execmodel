19f0274ac9ea0bcbd28edc9e4c11733ed0b8ab24
Paul Peregud <paulperegud@gmail.com>
2017-05-11 15:12:01 +0200

    fail subtask after second timeout

--- apps/execmodel/src/model.erl
+++ apps/execmodel/src/model.erl
@@ -1,31 +1,67 @@
 -module(model).
 
-%% API
+%%%%%%%%%%%%%%%%
+%%%%% API %%%%%%
+%%%%%%%%%%%%%%%%
 -export([run/2]).
 
 -type id() :: pos_integer().
--type res() :: oo | xx.
+-type res() :: oo | xx. %% oo - completed; xx - timeouted
+-type work() :: float().
+-type perf() :: float().
 
 -record(t, {
           id :: id(),
-          work :: float()
+          work :: work(),
+          h = [] :: [{id(), perf(), res()}]
          }).
 -type t() :: #t{}.
 
 -record(n, {
           id :: id(),
-          perf :: float(),
-          task :: undefined | t(),
-          r :: undefined | res(),
-          h = [] :: [{id(), float(), res()}],
-          t :: undefined | float()
+          perf :: perf(), %% performance of the node
+          task :: undefined | t(), %% current task
+          r :: undefined | res(), %% result of current task
+          h = [] :: [{id(), work(), res()}], %% history
+          t :: undefined | float() %% time of end of current computation
          }).
-%% -type n() :: #n{}.
 
 run(NS, NN) when NN =< NS ->
     Nodes = [prov() || _ <- lists:seq(1, NN)],
     STs = [st() || _ <- lists:seq(1, NS)],
     exec(Nodes, STs).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% generators and rules %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% RULE: for second try set subtask timeout to 2.0
+st_timeout(#t{h = H}) ->
+    max(1.0, math:pow(2, length(H))).
+
+%% RULE: retry subtask at most once
+st_status(#t{h = [{_, _, oo} | _]}) ->
+    completed;
+st_status(#t{h = H}) when length(H) < 2 ->
+    timeouted;
+st_status(#t{h = H}) when length(H) >= 2 ->
+    failed.
+
+%% RULE: place subtasks that time-outed at the end of the subtasks queue
+handle_timeouted(Tasks, TOs) ->
+    Tasks ++ TOs.
+
+%% generator: subtask is modeled as amount of CPU time needed to compute it
+st() ->
+    max(0, (rand:normal() + 1)).
+
+%% generator: provider is modeled as its computational power: [1,0..2,0]
+prov() ->
+    rand:uniform() + 1.
+
+%%%%%%%%%%%%%%%
+%% internals %%
+%%%%%%%%%%%%%%%
 
 exec(Nodes0, STs0) ->
     Nodes = [ #n{id = Id, perf = P}
@@ -44,23 +80,22 @@ loop(Done, Rest, Nodes0, Idle) ->
     Nodes1 = project(Nodes0), %% maybe computes values of r and t
     Nodes2 = lists:keysort(#n.t, Nodes1), %% ascending
     #n{t = Quantum} = hd(Nodes2), %% get closest t
-    io:fwrite("q: ~p~n", [Quantum]),
     Nodes3  = [ N#n{t = T - Quantum}
                 || N = #n{t = T} <- Nodes2 ], %% update time
     case reassign(Rest, Nodes3) of
-        {NewDone, [], Nodes4, Idle1} ->
-            loop(NewDone ++ Done, [], Nodes4, Idle1 ++ Idle);
-        {NewDone, Rest1, Nodes4, []} ->
-            loop(NewDone ++ Done, Rest1, Nodes4, Idle)
+        {NewDone, Failed, [], Nodes4, Idle1} ->
+            loop(Failed ++ NewDone ++ Done, [], Nodes4, Idle1 ++ Idle);
+        {NewDone, Failed, Rest1, Nodes4, []} ->
+            loop(Failed ++ NewDone ++ Done, Rest1, Nodes4, Idle)
     end.
 
 epsilon() ->
     0.000001.
 
 reassign(Tasks, Nodes) ->
-    {Compl, TOs, BusyProviders, FreeProviders} = finalize_execs(Nodes),
-    {Working, Idle, Rest} = assign(FreeProviders, TOs++Tasks),
-    {Compl, Rest, Working++BusyProviders, Idle}.
+    {Compl, TOs, Failed, BusyProviders, FreeProviders} = finalize_execs(Nodes),
+    {Working, Idle, Rest} = assign(FreeProviders, handle_timeouted(Tasks, TOs)),
+    {Compl, Failed, Rest, Working++BusyProviders, Idle}.
 
 finalize_execs(Nodes) ->
     E = epsilon(),
@@ -70,33 +105,34 @@ finalize_execs(Nodes) ->
            (_) -> false
         end,
     {Finishing, Working} = lists:partition(IsFree, Nodes),
-    R = [ {{R == xx, T}, N#n{task = undefined, r = undefined, t = undefined}}
-          || N = #n{task = T, r = R} <- Finishing ],
+    R = [ begin
+              #t{h = H} = T,
+              T1 = T#t{h = [{NId, Perf, R} | H]},
+              N1 = N#n{task = undefined, r = undefined, t = undefined},
+              {T1, N1}
+          end || N = #n{id = NId, perf = Perf, task = T, r = R} <- Finishing ],
     {ComplExecs, FreeProviders} = lists:unzip(R),
-    {Timeouted0, Finished0} = lists:partition(fun(X) -> element(1, X) end, ComplExecs),
-    {_, Timeouted} = lists:unzip(Timeouted0),
-    {_, Finished} = lists:unzip(Finished0),
-    {Finished, Timeouted, Working, FreeProviders}.
+    {Finished, Other} = lists:partition(fun(T) -> st_status(T) == completed end, ComplExecs),
+    {Timeouted, Failed} = lists:partition(fun(T) -> st_status(T) == timeouted end, Other),
+    {Finished, Timeouted, Failed, Working, FreeProviders}.
 
 project(Nodes) ->
     lists:map(fun schedule_work/1, Nodes).
 
-schedule_work(N = #n{perf = Perf, task = #t{id = TId, work = Work},
+schedule_work(N = #n{perf = Perf, task = Task,
                      t = undefined, h = H}) ->
+    #t{id = TId, work = Work} = Task,
     T = Work / Perf,
-    case T > st_deadline() of
+    case T > st_timeout(Task) of
         true ->
             NH = {TId, Work, xx},
-            N#n{t = st_deadline(), r = xx, h = [NH | H]};
+            N#n{t = st_timeout(Task), r = xx, h = [NH | H]};
         false ->
             NH = {TId, Work, oo},
             N#n{t = T, r = oo, h = [NH | H]}
     end;
 schedule_work(N = #n{task = #t{id = _Id}}) ->
     N.
-
-st_deadline() ->
-    1.0.
 
 assign(Nodes, STs) ->
     assign(Nodes, STs, []).
@@ -107,11 +143,3 @@ assign([], STs, Busy) ->
 assign(Idlers, [], Busy) ->
     {Busy, Idlers, []}.
 
-
-%% subtask is modeled as amount of CPU cycles needed to compute it
-st() ->
-    max(0, (rand:normal() + 1)).
-
-%% provider is modeled as its computational power: [1,0..2,0]
-prov() ->
-    rand:uniform() + 1.
